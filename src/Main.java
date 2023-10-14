6231efbbac90447dfda84886aae5768cf57434af
EugeniuPeruzzi <jaka1997@live.it>
2023-10-14 22:17:58 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String hello = "Hello World";
+		String secondVariable = " I'm using Eclipse IDE";
+		
+		int x = 2;
+		
+		System.out.println(hello + secondVariable + x);
+
+	}
+
+}
